b61377dae89de5afafb2ac905a22d2d503232ae4
Merthan <merthan.kavak@gmail.com>
2021-10-15 16:26:28 +0300

    make it better

--- app/controller/course/student/student_course_controller.js
+++ app/controller/course/student/student_course_controller.js
@@ -8,13 +8,14 @@ exports.show = async function (req, res) {
     try {
         const studentId = req.params.id;
 
-        const courseList = await Course.find([{
-            $match: {
-                '_id': {
-                    '$in': [studentId]
+        const courseList = await Course.find(
+            students, {
+                $match: {
+                    '_id': {
+                        '$in': [studentId]
+                    }
                 }
-            }
-        }]);
+            });
 
         if (!courseList) return res.status(401).json({
             message: 'Course does not exist.'
