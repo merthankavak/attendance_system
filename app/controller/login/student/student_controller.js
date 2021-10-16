34693d75e84e144ce8f0eec490f50621ba28acdb
Merthan <merthan.kavak@gmail.com>
2021-10-16 22:00:36 +0300

    make it better

--- app/controller/login/student/student_controller.js
+++ app/controller/login/student/student_controller.js
@@ -25,3 +25,48 @@ exports.changePassword = async (req, res) => {
         })
     }
 };
+
+// @route PUT api/student/{id}
+// @desc Update student details
+// @access Public
+exports.update = async function (req, res) {
+    try {
+        const newMimetype = req.file.mimetype;
+        const id = req.params.id;
+
+        const newImage = Buffer(fs.readFileSync(req.file.path).toString('base64'), 'base64');
+        fs.remove(req.file.path, (err) => {
+            if (err)
+                console.log(err);
+        });
+
+
+        // update picture only
+        Student.findByIdAndUpdate(
+            id, {
+                mimetype: newMimetype,
+                image: newImage
+            }, (err, student) => {
+                if (err) {
+                    res.status(500).json({
+                        message: err.message
+                    });
+                } else {
+                    const getImage = (image, mimetype) => {
+                        return image ? `data:${mimetype};base64,${Buffer(image).toString('base64')}` : '';
+                    }
+                    res.status(200).json({
+                        image: getImage(newImage, newMimetype),
+                        message: 'Student information updated successfully!'
+                    });
+
+                }
+
+            });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
--- app/routes/student/student.js
+++ app/routes/student/student.js
@@ -5,8 +5,26 @@ const {
 
 const Course = require('../../controller/course/student/student_course_controller');
 const Student = require('../../controller/login/student/student_controller');
-
+const multer = require('multer');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: (req, file, cb) => cb(null, new Date().toISOString() + '-' + file.originalname)
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 14
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+            cb(null, true);
+        else
+            cb('Only jpeg/jpg or png files!', false);
+    }
+});
 
 
 //POST UPDATE PASS
@@ -19,6 +37,8 @@ router.post('/changepassword/:id', [
     }) => (value === req.body.password)),
 ], Student.changePassword);
 
+//UPDATE
+router.put('/:id', upload, Student.update);
 
 //SHOW COURSE
 router.get('/course/:id', Course.show);
