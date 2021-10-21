9dbc259eef0176b9bd40c4fb4430e81f259f3f31
Merthan <merthan.kavak@gmail.com>
2021-10-22 00:33:05 +0300

    check attendance test

--- app/routes/teacher/teacher.js
+++ app/routes/teacher/teacher.js
@@ -2,12 +2,28 @@ const express = require('express');
 const {
     check
 } = require('express-validator');
-
+const multer = require('multer');
 
 const Teacher = require('../../controller/login/teacher/teacher_controller');
 const Course = require('../../controller/course/teacher/teacher_course_controller');
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: (req, file, cb) => cb(null, new Date().toISOString() + '-' + file.originalname)
+});
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 14
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg')
+            cb(null, true);
+        else
+            cb('Only jpeg/jpg or png files!', false);
+    }
+});
 
 //POST UPDATE PASSWORD
 router.post('/changepassword/:id', [
@@ -26,7 +42,7 @@ router.get('/course/:id', Course.show);
 router.post('/course/addcourse', Course.addCourse);
 
 //CHECK ATTENDANCE 
-router.post('/course/:id/checkattendance', Course.checkAttendance);
+router.post('/course/:id/checkattendance', upload.single('picture'), Course.checkAttendance);
 
 //EDIT COURSE SCHEDULE
 router.post('/course/:id/editschedule', Course.editCourseSchedule);
