1beec000f3decd1d1263b2cb55279b94718225b2
Merthan <merthan.kavak@gmail.com>
2021-10-22 14:02:13 +0300

    check attendance test

--- app/controller/course/teacher/teacher_course_controller.js
+++ app/controller/course/teacher/teacher_course_controller.js
@@ -149,20 +149,14 @@ exports.checkAttendance = async (req, res) => {
         var image = Buffer(fs.readFileSync(file.path).toString('base64'), 'base64');
 
         let studentsArray = currentCourse.attendance[0].students;
-        //var imageByteArray = [];
+        var imageByteArray = [];
 
-
-        var i, j, imageByteArray = [];
-        if (!imageByteArray[i]) imageByteArray[i] = [];
-
-
-        for (i = 0; i < studentsArray.length; i++) {
-
+        for (let i = 0; i < studentsArray.length; i++) {
             var studentId = studentsArray[i].id;
             var student = await Student.findById(studentId);
             var studentImageArray = student.image;
-            for (j = 0; j < studentImageArray.length; j++) {
-                imageByteArray[i][j] = studentImageArray[j].imageByte;
+            for (let j = 0; j < studentImageArray.length; j++) {
+                imageByteArray[i] = imageByteArray[i].push(studentImageArray[j].imageByte);
             }
 
             var faceData = await rekognition.compareFaces({
