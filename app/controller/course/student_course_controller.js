11962979d9616eea7218bb9c8a96b8d494d7791a
Merthan <merthan.kavak@gmail.com>
2021-10-27 12:49:29 +0300

    get student course

--- app/controller/course/student_course_controller.js
+++ app/controller/course/student_course_controller.js
@@ -99,9 +99,7 @@ exports.show = async function (req, res) {
                 teacher: course.teacher.teacherName,
                 courseShortName: course.courseShortName,
                 courseName: course.courseName,
-                students: course.students.select({
-                    studentName
-                }),
+                students: course.students.find({}, 'studentName')
             }
         });
 
