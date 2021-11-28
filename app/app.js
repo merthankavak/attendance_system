da8065a777279984f98a844a0d76aeadb8d45c32
Merthan <merthan.kavak@gmail.com>
2021-11-28 21:41:27 +0300

    fix model

--- app/app.js
+++ app/app.js
@@ -9,9 +9,12 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use(express.urlencoded({
+    limit: '50mb',
     extended: true
 }));
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 app.use(cors());
 app.use(bodyParser({
     limit: '50mb'
