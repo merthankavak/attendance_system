d93b13359d0379f313dd0fa9326093ce12fe5906
Merthan <merthan.kavak@gmail.com>
2021-11-28 19:04:54 +0300

    fix model

--- app/controller/course/teacher_course_controller.js
+++ app/controller/course/teacher_course_controller.js
@@ -167,7 +167,7 @@ exports.takeAttendance = async (req, res) => {
             message: 'Course does not exist'
         });
 
-        var image = req.body.file;
+        var image = req.body.image;
 
         if (!image) return res.status(401).json({
             message: 'You must upload at least one image'
