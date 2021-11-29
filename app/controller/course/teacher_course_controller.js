f1a8af6a666f514fce45d5e9c623b0dffbe0ec97
Merthan <merthan.kavak@gmail.com>
2021-11-29 11:34:08 +0300

    fix model

--- app/controller/course/teacher_course_controller.js
+++ app/controller/course/teacher_course_controller.js
@@ -166,10 +166,9 @@ exports.takeAttendance = async (req, res) => {
         if (!currentCourse) res.status(401).json({
             message: 'Course does not exist'
         });
-        var imageJson = JSON.stringify(data);
-        var imageString = String(imageJson);
+        var imageString = String(data);
         ba64.writeImageSync("myimage", imageString); // Saves myimage.jpeg.
-        var imageMime = imageString.toString().split('data:image/')[0];
+        var imageMime = String(data).split('data:image/')[0];
         var imageFile = Buffer(fs.readFileSync(imageString + '.' + imageMime).toString('base64'), 'base64');
         if (imageFile == '') return res.status(401).json({
             message: 'You must upload at least one image'
