a66c978a070af0fcb1d9b0c6850122b878b60e6e
Merthan <merthan.kavak@gmail.com>
2021-11-29 15:50:25 +0300

    fix model

--- app/controller/course/teacher_course_controller.js
+++ app/controller/course/teacher_course_controller.js
@@ -170,7 +170,7 @@ exports.takeAttendance = async (req, res) => {
 
         function getBinary(base64image) {
 
-            var binaryImage = atob(base64image);
+            var binaryImage = Buffer.from(base64image, 'base64')
             var length = binaryImage.length;
             var ab = ArrayBuffer(length);
             var ua = Uint8Array(ab);
