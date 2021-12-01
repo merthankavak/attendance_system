aac44d10e88c86fd7229b8839174811b94b8988e
Merthan <merthan.kavak@gmail.com>
2021-12-01 17:52:25 +0300

    fix model

--- app/app.js
+++ app/app.js
@@ -11,16 +11,6 @@ require('dotenv').config();
 app.use(bodyParser.json({
     limit: '50mb'
 }));
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    extended: true,
-    parameterLimit: 50000
-}))
-app.use(express.urlencoded({
-    limit: '50mb',
-    extended: true,
-    parameterLimit: 50000
-}));
 app.use(express.json({
     limit: '50mb'
 }));
