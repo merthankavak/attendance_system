882968c6514012666ece332212feda3838833599
Merthan <merthan.kavak@gmail.com>
2021-12-11 22:31:54 +0300

    fix model

--- app/routes/teacher/teacher_route.js
+++ app/routes/teacher/teacher_route.js
@@ -50,9 +50,7 @@ router.delete('/course/deletecourse/:id', Course.deleteCourse);
 router.post('/course/addschedule/:id', Course.addCourseSchedule);
 
 //Teacher Check Course Attendance
-router.post('/course/takeattendance/:id/:date', upload.fields([{
-    name: 'image'
-}]), Course.takeAttendance);
+router.post('/course/takeattendance/:id/:date', upload.single('image'), Course.takeAttendance);
 
 //Teacher Manage Course Attendance
 router.post('/course/manageattendance/:id/:date', Course.manageAttendance);
