ced28c4b471275e6b51a766212da29d26fe18b60
Merthan <merthan.kavak@gmail.com>
2021-12-11 21:15:36 +0300

    fix model

--- app/routes/teacher/teacher_route.js
+++ app/routes/teacher/teacher_route.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
             cb(null, true);
         } else {
             cb('Only jpeg/jpg or png files!', false);
