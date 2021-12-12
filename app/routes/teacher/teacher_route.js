bbf7fa95ed62b3d015f28d71a1a2230c05904576
Merthan <merthan.kavak@gmail.com>
2021-12-12 13:06:44 +0300

    fix model

--- app/routes/teacher/teacher_route.js
+++ app/routes/teacher/teacher_route.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 });*/
 const upload = multer({
     storage: multerS3({
-        //acl: "public-read",
+        acl: "public-read",
         s3,
         bucket: "attendancesystembucket",
         key: function (req, file, cb) {
